d6c8a7058c09082472bab1ace79366c1b72c5f19
akramfdk <akramjeet.singh@gmail.com>
2025-08-28 12:34:29 +1000

    Added a button to generate random colors

--- background_generator/script.js
+++ background_generator/script.js
@@ -3,6 +3,16 @@ let color2 = document.querySelector("#color2");
 let body = document.body;
 let text = document.querySelector("h3");
 let select = document.querySelector("select");
+let random_button = document.querySelector("#random-color");
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function updateBackground(){
     let colorValue = `linear-gradient(${select.value}, ${color1.value}, ${color2.value})`;
@@ -10,8 +20,14 @@ function updateBackground(){
     text.textContent = body.style.background + ";";
 }
 
+function generateRandomBackground(){
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+    updateBackground();
+}
 
 color1.addEventListener("input", updateBackground);
 color2.addEventListener("input", updateBackground);
 select.addEventListener("input", updateBackground);
-updateBackground();
+random_button.addEventListener("click", generateRandomBackground);
+generateRandomBackground();
