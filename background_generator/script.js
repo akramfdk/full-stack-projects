1782536520e5a99a9486ac09bae5ae9caaeabe62
akramfdk <akramjeet.singh@gmail.com>
2025-09-02 12:34:24 +1000

    Upgraded to latest ES version

--- background_generator/script.js
+++ background_generator/script.js
@@ -1,32 +1,32 @@
-let color1 = document.querySelector("#color1");
-let color2 = document.querySelector("#color2");
-let body = document.body;
-let text = document.querySelector("h3");
-let select = document.querySelector("select");
-let random_button = document.querySelector("#random-color");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const body = document.body;
+const text = document.querySelector("h3");
+const select = document.querySelector("select");
+const random_button = document.querySelector("#random-color");
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+getRandomColor = () => {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
 }
 
-function updateBackground(){
+updateBackground = () => {
     let colorValue = `linear-gradient(${select.value}, ${color1.value}, ${color2.value})`;
     body.style.background = colorValue;
     text.textContent = body.style.background + ";";
 }
 
-function generateRandomBackground(){
+generateRandomBackground = () => {
     color1.value = getRandomColor();
     color2.value = getRandomColor();
     updateBackground();
 }
 
-function copyToClipboard(){
+copyToClipboard = () => {
     navigator.clipboard.writeText(text.textContent);
     alert("Gradient string to Clipboard");
 }
